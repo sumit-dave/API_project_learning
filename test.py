382e1ffc856e9b2dc839b8164917f421de00a04c
sumit-dave <sumit@gmail.com>
2023-07-30 15:12:18 +0530

    Flask

---
+++ test.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+
+app=Flask(__name__)
+
+## GET means, we are posting a data in URL.
+## POST means, we are posting a data in body.
+
+@app.route('/abc', methods=['GET','POST'])
+def test1():
+    if(request.method=='POST'):
+        a=request.json['num1']
+        b=request.json['num2']
+        result=a+b
+        return jsonify(str(result))
+
+@app.route('/efg/sumit', methods=['GET','POST'])
+def test2():
+    if (request.method == 'POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a * b
+        return jsonify(str(result))
+
+
+if __name__=='__main__':    ##invoke the Entire python project main classes
+    app.run()
+
+
+
